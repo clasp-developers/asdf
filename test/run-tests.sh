a7ae69ab31c657e983502e528e151115599720a2
Tarn W. Burton <twburton@gmail.com>
2023-05-27 11:03:13 -0400

    Fix typo

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -381,7 +381,7 @@ upgrade_tags () {
 
     if [ "$lisp" = cmucl ]; then
         echo REQUIRE 3.3.2 3.3.1 3.3.0
-    else if [ "$lisp" = clasp ]; then
+    elif [ "$lisp" = clasp ]; then
         echo REQUIRE 3.3.2 3.3.1 3.3.0 3.2.1 3.2.0
     else
         echo REQUIRE 3.3.2 3.3.1 3.3.0 3.2.1 3.2.0 3.1.7 3.1.2 3.0.3 2.26
